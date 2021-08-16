bf7ca0b739b892f1513dd93a93e506bf825d6849
Michał Radziwiłko <michal4237@gmail.com>
2021-08-16 12:08:19 +0200

    Some minor layout fixes

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -14,19 +14,20 @@ const TransactionList = () => {
   return (
     <div>
       {isAddTransactionModalOpen && <AddTransactionModal />}
-      <button className="add-btn" onClick={openAddTransactionModal}>
-        +
-      </button>
+
       {alert.show && (
         <Alert type={alert.type} msg={alert.msg} list={transactions} />
       )}
       <div
         className={`list-container ${
-          transactions.length !== 0 && "transaction-list"
+          transactions.length !== 0 ? "transaction-list" : ""
         }`}
       >
+        <button className="add-btn" onClick={openAddTransactionModal}>
+          +
+        </button>
         {transactions.length === 0 && (
-          <h3>no transactions (click the "+" button to add transactions</h3>
+          <h3>no transactions (click the "+" button to add transactions)</h3>
         )}
         {transactions &&
           transactions.map((transaction) => {
